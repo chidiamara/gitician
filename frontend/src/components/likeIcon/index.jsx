458ccc1951c3a71ebc5f23be3ee22059910e4909
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-18 20:04:12 +0000

    created likes icon

--- frontend/src/components/likeIcon/index.jsx
+++ frontend/src/components/likeIcon/index.jsx
@@ -1,6 +1,11 @@
+import { AiFillLike } from "react-icons/ai";
+
 const LikesIcon = () => {
   return (
-    <div>LikesIcon</div>
+    <div className='flex justify-center transition-colors duration-200 rounded-lg hover:bg-gray-800 group relative'>
+                <AiFillLike size={22}/>
+                <span className="opacity-0 group-hover:opacity-100 absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-2 py-1 rounded-full text-xs p-1.5 ">Likes</span>
+    </div>
   )
 }
 
