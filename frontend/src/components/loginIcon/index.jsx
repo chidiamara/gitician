52385970ce4c51bd8e195a94b6a56a24e54c0834
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-18 20:17:32 +0000

    created login icon

--- frontend/src/components/loginIcon/index.jsx
+++ frontend/src/components/loginIcon/index.jsx
@@ -1,6 +1,11 @@
+import { RiLoginCircleLine } from "react-icons/ri";
+
 const LoginIcon = () => {
   return (
-    <div>LoginIcon</div>
+    <div className='flex justify-center transition-colors duration-200 rounded-lg hover:bg-gray-800 group relative'>
+                <RiLoginCircleLine size={22}/>
+                <span className="opacity-0 group-hover:opacity-100 absolute bottom-0 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white px-2 py-1 rounded-full text-xs p-1.5 ">Login</span>
+    </div>
   )
 }
 
