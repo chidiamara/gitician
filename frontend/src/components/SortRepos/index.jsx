61e9aac684fddd53a6820b0b6187356144ec409a
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-20 00:49:42 +0000

    created sort repository component

--- frontend/src/components/SortRepos/index.jsx
+++ frontend/src/components/SortRepos/index.jsx
@@ -1,6 +1,25 @@
 const SortRepos = () => {
   return (
-    <div>SortRepos</div>
+    <div className='mb-2 flex justify-center lg:justify-end'>
+			<button
+				type='button'
+				className={`py-2.5 px-5 me-2 mb-2 text-nxs sm:text-sm font-medium focus:outline-none rounded-lg glass-background border-[#593A5A]`}
+			>
+				Most Recent
+			</button>
+            <button
+				type='button'
+				className={`py-2.5 px-5 me-2 mb-2  text-nxs sm:text-sm font-medium focus:outline-none rounded-lg glass-background border-[#593A5A]`}
+			>
+				Most Forks
+			</button>
+			<button
+				type='button'
+				className={`py-2.5 px-5 me-2 mb-2  text-nxs sm:text-sm font-medium focus:outline-none rounded-lg glass-background border-[#593A5A]`}
+			>
+				Most Stars
+			</button>
+		</div>
   )
 }
 
--- frontend/src/pages/Home/page.jsx
+++ frontend/src/pages/Home/page.jsx
@@ -1,9 +1,16 @@
 import Search from '../../components/Search'
+import SortRepos from '../../components/SortRepos'
 
 const Home = () => {
   return (
     <>
+    <div className='m-4'>
     <Search/>
+    <SortRepos/>
+    <div className='flex flex-col lg:flex-row justify-center items-start gap-4'>
+      profile info
+    </div>
+    </div>
     </>
   )
 }
