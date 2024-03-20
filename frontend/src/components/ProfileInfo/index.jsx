25900f506e00e83fddf4d9ff29c7d5578797c7d9
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-20 00:56:23 +0000

    created profile info component

--- frontend/src/components/ProfileInfo/index.jsx
+++ frontend/src/components/ProfileInfo/index.jsx
@@ -1,6 +1,31 @@
 const ProfileInfo = () => {
+
+  const userProfile = {
+		avatar_url: "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
+		bio: "👨🏻‍💻👨🏻‍💻👨🏻‍💻",
+		email: "maranara@gmail.com",
+		followers: 345,
+		following: 42,
+		html_url: "https://github.com/openai",
+		location: "Under the Sun",
+		name: "Mara Nara",
+		public_gists: 34,
+		public_repos: 21,
+		twitter_username: "maranara",
+		login: "maranara",
+	};
+
   return (
-    <div>ProfileInfo</div>
+    <div className='lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10'>
+			<div className='glass-background rounded-lg p-4 border-[#593A5A]'>
+				<div className='flex gap-4 items-center'>
+					{/* Avatar For User */}
+					<a href={userProfile?.html_url} target='_blank' rel='noreferrer'>
+						<img src={userProfile?.avatar_url} className='rounded-md w-24 h-24 mb-2' alt='' />
+					</a>
+				</div>
+			</div>
+		</div>
   )
 }
 
--- frontend/src/pages/Home/page.jsx
+++ frontend/src/pages/Home/page.jsx
@@ -1,5 +1,6 @@
 import Search from '../../components/Search'
 import SortRepos from '../../components/SortRepos'
+import ProfileInfo from '../../components/ProfileInfo'
 
 const Home = () => {
   return (
@@ -7,9 +8,9 @@ const Home = () => {
     <div className='m-4'>
     <Search/>
     <SortRepos/>
-    <div className='flex flex-col lg:flex-row justify-center items-start gap-4'>
-      profile info
-    </div>
+      <div className='flex flex-col lg:flex-row justify-center items-start gap-4'>
+        <ProfileInfo/>
+      </div>
     </div>
     </>
   )
