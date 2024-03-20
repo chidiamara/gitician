c2cbd818055f778140c9f93012aa13f59a6a1854
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-20 00:13:50 +0000

    created signup page

--- frontend/src/pages/SignUp/page.jsx
+++ frontend/src/pages/SignUp/page.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import { FaGithub, FaUnlockAlt } from "react-icons/fa";
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className='flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0'>
+			<div className='w-full glass-background rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0'>
+				<div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
+					<h1 className='text-xl font-bold md:text-2xl text-center'>Create Account</h1>
+					<button
+						type='button'
+						className='text-white bg-[#593A5A] hover:bg-[#593A5A]/90 focus:ring-4
+						focus:ring-[#593A5A]/50 font-medium rounded-lg flex gap-2 p-2 items-center w-full
+						text-center justify-center'
+					>
+						<FaGithub className='w-5 h-5' />
+						Sign up with Github
+					</button>
+					<p className='text-gray-300 text-center'>
+						Sign up and unlock
+            <span>
+							<FaUnlockAlt className='w-4 h-4 inline mx-2' />
+						</span> all app features.
+					</p>
+					<p className='text-sm font-light text-gray-400 text-center'>
+						Already have an account?{" "}
+						<Link to='/login' className='font-medium text-primary-600 hover:underline text-blue-600'>
+							Login
+						</Link>
+					</p>
+				</div>
+			</div>
+		</div>
   )
 }
 
