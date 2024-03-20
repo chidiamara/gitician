a8a1df6d5e0447faf3c9ef537be40a9010b99d16
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-20 00:38:35 +0000

    created explore page

--- frontend/src/pages/Explore/page.jsx
+++ frontend/src/pages/Explore/page.jsx
@@ -1,6 +1,39 @@
 const Explore = () => {
   return (
-    <div>Explore</div>
+    <div className='px-4 mt-4'>
+      <div className='max-w-2xl mx-auto rounded-md p-4 glass-background border-[#593A5A]'>
+        <h1 className='text-xl font-bold text-center'>Let's Explore Popular Repositories</h1>
+        <div className='flex flex-wrap justify-center gap-2 my-2'>
+          <img src='/svg/java.svg' alt='java logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/javascript.svg' alt='javascript logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/typescript.svg' alt='typescript logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/python.svg' alt='python logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/c.svg' alt='c logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/c++.svg' alt='c++ logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/csharp.svg' alt='csharp logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/html.svg' alt='html logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/css.svg' alt='css logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/bash.svg' alt='bash logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/go.svg' alt='go logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/swift.svg' alt='swift logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/ruby.svg' alt='ruby logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/php.svg' alt='php logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/rust.svg' alt='rust logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/kotlin.svg' alt='kotlin logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/scala.svg' alt='scala logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/perl.svg' alt='perl logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/r.svg' alt='r logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/objectivec.svg' alt='objectivec logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/dart.svg' alt='dart logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/coffeescript.svg' alt='coffeescript logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/erlang.svg' alt='erlang logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/elixir.svg' alt='elixir logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/haskell.svg' alt='haskell logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/lua.svg' alt='lua logo' className='h-11 sm:h-20 cursor-pointer'/>
+          <img src='/svg/clojure.svg' alt='clojure logo' className='h-11 sm:h-20 cursor-pointer'/>
+        </div>
+      </div>
+    </div>
   )
 }
 
