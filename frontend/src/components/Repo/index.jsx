6c4445eaabf7aa77ae8c7db4c3f45a9c341c6c2c
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-22 02:13:34 +0000

    Github user display
    fork count, star count, clone icon
    programming language used

--- frontend/src/components/Repo/index.jsx
+++ frontend/src/components/Repo/index.jsx
@@ -1,4 +1,5 @@
-import { FaCodeBranch } from "react-icons/fa";
+import { FaCodeBranch, FaCopy, FaStar } from "react-icons/fa";
+import { FaCodeFork } from "react-icons/fa6";
 
 const Repo = () => {
   return (
@@ -6,7 +7,7 @@ const Repo = () => {
     <span className="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-md -start-3 ring-8 ring-white">
         <FaCodeBranch className="w-5 h-5 text-gray-800"/>
     </span>
-    <div>
+    <div className="flex gap-2 flex-wrap items-center">
         <a href={"https://github.com/chidiamara/expense-tracker"}
         target="_blank"
         rel="noreferrer"
@@ -14,7 +15,23 @@ const Repo = () => {
         >
             Expense Tracker
         </a>
+
+        <span className="bg-purple-100 text-purple-800 text-nxs font-medium px-2.5 py-0.5 rounded-full flex items-center gap-1">
+                <FaCodeFork/> 32
+            </span>
+            <span className="bg-yellow-100 text-yellow-600 text-nxs font-medium px-2.5 py-0.5 rounded-full flex items-center gap-1">
+                <FaStar/> 89
+            </span>
+            <span className="cursor-pointer bg-green-100 text-green-900 text-nxs font-medium px-2.5 py-0.5 rounded-full flex items-center gap-1">
+                <FaCopy/> Clone
+        </span>
     </div>
+
+    <time className="block my-1 text-nxs font-normal leading-none text-gray-400">
+            Released on March 2022
+    </time>
+        <p>Expense Tracker | HTML, CSS and JavaScript </p>
+        <img src={"/svg/css.svg"} alt="Programming language icon" className="h-8" />
 </li>
   )
 }
