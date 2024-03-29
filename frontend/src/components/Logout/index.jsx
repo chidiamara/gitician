1e0bba0cf3f40b23435659bfd9fc6a4967e72fb9
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-29 02:25:12 +0000

    added the complete logout functionality

--- frontend/src/components/Logout/index.jsx
+++ frontend/src/components/Logout/index.jsx
@@ -1,14 +1,30 @@
 
+import toast from "react-hot-toast";
 import { RiLogoutCircleLine } from "react-icons/ri";
+import { useAuthContext } from "../../Context/auth";
+import User from "../../../../server/models/user.model";
 
 const Logout = () => {
+	const {userAuth, setAuthUser} = useAuthContext();
+
+	const logoutHandler = async () => {
+		try {
+			const res = await fetch("/api/auth/logout", {credentials: "include"});
+			const data = await res.json();
+			console.log(data);
+			setAuthUser(null);
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
   return (
     <>
 			<img
-				src={"https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"}
+				src={userAuth.avatarUrl}
 				className='w-10 h-10 rounded-full border border-[#593A5A]'
 			/>
             <div className='cursor-pointer flex justify-center transition-colors duration-200 rounded-lg hover:bg-[#593A5A] group relative'
+				onClick={logoutHandler}
                 >
 
 				<RiLogoutCircleLine size={22}/>
