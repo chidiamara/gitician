468a60c86207667d494e7fd3b3c6a74fe9bb950b
Chidi <chidiamaraekejiuba@gmail.com>
2024-03-29 05:20:00 +0000

    full activation of like profile functionality

--- frontend/src/components/LikeProfile/index.jsx
+++ frontend/src/components/LikeProfile/index.jsx
@@ -1,9 +1,29 @@
 import { AiFillLike } from 'react-icons/ai'
+import toast from 'react-hot-toast';
+import { useAuthContext } from '../../Context/auth';
 
-const LikeProfile = () => {
-  const profileLikeHandler = () => {
-    console.log('Profile Liked')
-  }
+const LikeProfile = ({userProfile}) => {
+
+  const {userAuth} = useAuthContext();
+  const isOwnProfile = userAuth?.username === userProfile.login;
+
+  const profileLikeHandler = async () => {
+    try {
+      const res = await fetch(`/api/users/like/${userProfile.login}`, {
+        method: "POST",
+        credentials: "include",
+      });
+      const data = await res.json();
+
+      if(data.error) throw new Error(data.error);
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  if (!userAuth || isOwnProfile) return null;
+
   return (
     <button className='glass-background font-medium w-full text-nxs p-2 rounded-md cursor-pointer border border-[#593A5A] flex items-center gap-2'
     onClick={profileLikeHandler}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import { getUserProfAndRepos } from '../controllers/user.controller.js';
+import { guaranteeAuthenticated } from '../middleware/guaranteeAuthenticated.js';
+import { getLikes, likeProfile } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/profile/:username', getUserProfAndRepos);
+
+router.get('/likes', guaranteeAuthenticated, getLikes);
+router.post('/like/:username', guaranteeAuthenticated, likeProfile);
 
 export default router;
