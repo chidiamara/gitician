7a39b288a9b8ede4ca0542b045e45dc5315a096a
Chidi <chidiamaraekejiuba@gmail.com>
2024-04-01 03:43:02 +0100

    explore route authentication

--- server/routes/explore.route.js
+++ server/routes/explore.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { explorePopularRepos } from '../controllers/explore.controller.js';
+import { guaranteeAuthenticated } from '../middleware/guaranteeAuthenticated.js';
 
 const router = express.Router();
 
-router.get('/repos/:language', explorePopularRepos);
+router.get('/repos/:language', guaranteeAuthenticated, explorePopularRepos);
 
 export default router;
